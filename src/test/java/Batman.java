848b4e5e96a97d1b862ac7db214adc222aa83250
Rozanaina <rozaaitbaeva@gmail.com>
2020-11-01 10:24:06 -0600

    have cool bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,5 +3,6 @@ public class Batman {
         System.out.println("I will catch you Joker");
         // yes we will catch them
         //batma gas some cool mobil
+        // batman has cool motocikle
     }
 }
